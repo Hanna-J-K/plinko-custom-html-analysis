b621b036bc84a1c2b86875b0f6a5f71d9fb51f22
Hanna-J-K <freeelanc3r@gmail.com>
2024-03-07 10:43:11 +0100

    flip conditions on certain methods update excel format

--- index.js
+++ index.js
@@ -13,65 +13,7 @@ XLSX.set_fs(fs);
 import { Readable } from "stream";
 XLSX.stream.set_readable(Readable);
 
-import {
-  checkForGoogleFonts,
-  checkForBootstrapImports,
-  testMethodAlwaysFalse,
-  checkForBootstrapClasses,
-  checkForAEMGridClasses,
-  hasImgWithoutLazyLoading,
-  hasImgWithoutAlt,
-  hasIncorrectVideoPlayerImport,
-  hasIframeTag,
-  hasStyleAttribute,
-  hasUniqueParentElementId,
-  hasBodyOrHeadInFragment,
-  hasH4TagsAndBelow,
-  hasTagsAboveH4,
-  tablesHasCaption,
-  searchForSocialSharingElements,
-  imgHasSizeParameters,
-} from "./methods.js";
-
-const checkList = [
-  { method: checkForGoogleFonts, description: "Has Google Fonts imports" },
-  {
-    method: checkForBootstrapImports,
-    description: "Has Bootstrap css imports",
-  },
-  { method: checkForBootstrapClasses, description: "Has Bootstrap classes" },
-  { method: checkForAEMGridClasses, description: "Has AEM Grid classes" },
-  {
-    method: hasImgWithoutLazyLoading,
-    description: "Has img without lazy-loading class",
-  },
-  { method: hasImgWithoutAlt, description: "Has img without alt attribute" },
-  {
-    method: imgHasSizeParameters,
-    description: "All Img src have size parameters",
-  },
-  {
-    method: hasIncorrectVideoPlayerImport,
-    description: "Has incorrect video player import",
-  },
-  { method: hasStyleAttribute, description: "Has style attribute" },
-  {
-    method: hasUniqueParentElementId,
-    description: "Has unique parent element id",
-  },
-  {
-    method: hasBodyOrHeadInFragment,
-    description: "Has <body> or <head> in fragment",
-  },
-  { method: hasH4TagsAndBelow, description: "Has h4 tags and below (h5,h6)" },
-  { method: hasTagsAboveH4, description: "Has tags above h4 (h1,h2,h3)" },
-  { method: tablesHasCaption, description: "Tables has caption" },
-  {
-    method: searchForSocialSharingElements,
-    description: "Has social sharing elements",
-  },
-  { method: hasIframeTag, description: "Has iframe" },
-];
+import { checkList } from "./checklist.js";
 
 let workbook;
 fs.access("PageResults.xlxs", fs.constants.F_OK, (err) => {
@@ -82,12 +24,21 @@ fs.access("PageResults.xlxs", fs.constants.F_OK, (err) => {
   }
 });
 
-const excelHeader = ["RULE DESCRIPTION", "CHECK RESULT", "VALUE (if passed)"];
+const excelHeader = [
+  "NO",
+  "SECTION",
+  "CHECKPOINT",
+  "STATUS",
+  "PRIORITY",
+  "DONE",
+  "COMMENTS",
+  "FLUID COMMENTS",
+];
 
 const createResultsWorkbook = (results, url) => {
   const worksheet = XLSX.utils.json_to_sheet(results);
 
-  XLSX.utils.book_append_sheet(workbook, worksheet, "Page" + urls.indexOf(url));
+  XLSX.utils.book_append_sheet(workbook, worksheet, `Page ${urls.indexOf(url) + 1}`);
   XLSX.utils.sheet_add_aoa(worksheet, [excelHeader], { origin: "A1" });
 
   let wscols = [];
@@ -104,15 +55,54 @@ const runProcess = async () => {
     const response = await axios.get(url);
     const website = cheerio.load(response.data);
 
-    let results = [{description: undefined, result: undefined, value: undefined}];
-    checkList.forEach(({ method, description }) => {
-      const { result, value } = method(website);
-      results.push({ description, result, value });
+    let results = [];
+    let methodNumber = 0;
+    checkList.forEach(
+      ({
+        section,
+        method,
+        checkpoint,
+        priority,
+        done,
+        comments,
+        fluidComments,
+      }) => {
+        const { result, value } = method(website);
+        methodNumber += 1;
+        comments = result == "FAIL" ? comments : ""
+        results.push({
+          methodNumber,
+          section,
+          checkpoint,
+          result,
+          priority,
+          done,
+          comments,
+          fluidComments,
+        });
+        console.log({checkpoint, result, value})
+      }
+    );
+    results.push({
+      methodNumber: undefined,
+      section: undefined,
+      checkpoint: undefined,
+      result: undefined,
+      priority: undefined,
+      done: undefined,
+      comments: undefined,
+      fluidComments: undefined,
     });
-    results.push({description: undefined, result: undefined, value: undefined})
-    results.push({description: "PAGE URL", result: undefined, value: url})
+    results.push({ methodNumber: undefined,
+      section: "PAGE URL",
+      checkpoint: url,
+      result: undefined,
+      priority: undefined,
+      done: undefined,
+      comments: undefined,
+      fluidComments: undefined, });
 
-    console.table(results);
+    
 
     createResultsWorkbook(results, url);
   }
--- methods.js
+++ methods.js
@@ -3,66 +3,67 @@ const globalSelector = '.html-fragment-content ';
 const checkForGoogleFonts = ($) => {
     for(const element of $(globalSelector + 'link')) {
         if ($(element).attr('href').includes('fonts.googleapis.com'))
-            return { result : true, value : $(element).attr('href') };
+            return { result : "FAIL", value : $(element).attr('href') };
     }
-    return { result : false };
+    return { result : "PASS" };
 }
 
 const checkForBootstrapImports = ($) => {
     for(const element of $(globalSelector + 'link')) {
         if ($(element).attr('href').includes('bootstrap'))
-            return { result : true, value : $(element).attr('href') };
+            return { result : "PASS", value : $(element).attr('href') };
     }
-    return { result : false };
+    return { result : "FAIL" };
 }
 
 const checkForBootstrapClasses = ($) => {
     if ($(globalSelector + '*[class*="col-md"]').length > 0) {
-        return { result : true, value : $(globalSelector + '*[class*="col-md"]').attr('class')};
+        return { result : "PASS", value : $(globalSelector + '*[class*="col-md"]').attr('class')};
     }
-    return { result : false };
+    return { result : "FAIL" };
 }
 
 const checkForAEMGridClasses = ($) => {
     if ($(globalSelector + '*[class*="aem-Grid"]').length > 0) {
-        return { result : true, value : $(globalSelector + '*[class*="aem-Grid"]').attr('class')};
+        return { result : "PASS", value : $(globalSelector + '*[class*="aem-Grid"]').attr('class')};
     }
-    return { result : false };
+    return { result : "FAIL" };
 }
 
 const hasImgWithoutLazyLoading = ($) => {
     if ($(globalSelector + 'img[class*="lazy-image"]').length === 0) {
-        return { result : true, value : $(globalSelector + 'img[class*="lazy-image"]').length};
+        return { result : "PASS", value : $(globalSelector + 'img[class*="lazy-image"]').length};
     }
-    return { result : false };
+    return { result : "FAIL" };
 }
 
+// this needs to be expanded with checkpoints for decorative images
 const hasImgWithoutAlt = ($) => {
     if ($(globalSelector + 'img:not([alt])').length > 0) {
-        return { result : true, value : $(globalSelector + 'img:not([alt])').length};
+        return { result : "FAIL", value : $(globalSelector + 'img:not([alt])').length};
     }
-    return { result : false };
+    return { result : "PASS" };
 }
 
 const imgHasSizeParameters = ($) => {
-    let allImagesOK = false;
+    let allImagesOK = "FAIL";
     for(const element of $(globalSelector + 'img')) {
         if ($(element).attr('src').includes('wid=') || $(element).attr('src').includes('hei=')) {
-            allImagesOK = true;
+            allImagesOK = "PASS";
         } else {
-            allImagesOK = false;
+            allImagesOK = "FAIL";
         }
     }
     return { result : allImagesOK };
 }
 
 const hasIncorrectVideoPlayerImport = ($) => {
-    let result = false;
+    let result = "FAIL";
     if ($(globalSelector + 'video').length > 0 || $(globalSelector + '*[video-id]').length > 0 || $(globalSelector + '*[data-video-id]').length > 0){
-        result = true;
+        result = "FAIL";
         for(const element of $(globalSelector + 'link')) {
             if ($(element).attr('href').includes('content/dam/assets-shared/custom/third-party-scripts/js/video-8.3.0.js')) {
-                result = false;
+                result = "PASS";
             }
         }
     }
@@ -71,24 +72,24 @@ const hasIncorrectVideoPlayerImport = ($) => {
 
 const hasIframeTag = ($) => {
     if ($(globalSelector + 'iframe').length > 0) {
-        return { result : true, value : $(globalSelector + 'iframe').attr('src')};
+        return { result : "FAIL", value : $(globalSelector + 'iframe').attr('src')};
     }
-    return { result : false };
+    return { result : "PASS" };
 }
 
 const hasStyleAttribute = ($) => {
     if ($(globalSelector + '*[style]').length > 0) {
-        return { result : true, value : $(globalSelector + '*[style]').attr('style')};
+        return { result : "FAIL", value : $(globalSelector + '*[style]').attr('style')};
     }
-    return { result : false };
+    return { result : "PASS" };
 }
 
 const hasUniqueParentElementId = ($) => {
-    let result = false;
+    let result = "FAIL";
     if ($(globalSelector).attr('id')) {
-        result = true;
+        result = "PASS";
         if ($('#' + $(globalSelector).attr('id')).length >= 1) {
-            result = false;
+            result = "FAIL";
         }
     }
     return { result : result, value : $(globalSelector).attr('id')};
@@ -96,42 +97,42 @@ const hasUniqueParentElementId = ($) => {
 
 const hasBodyOrHeadInFragment = ($) => {
     if (($(globalSelector + 'body').length >= 1) || ($(globalSelector + 'head').length >= 1)) {
-        return { result : true };
+        return { result : "FAIL" };
     }
-    return { result : false };
+    return { result : "PASS" };
 }
 
 const hasH4TagsAndBelow = ($) => {
     if ($(globalSelector + 'h4').length > 0 || $(globalSelector + 'h5').length > 0 || $(globalSelector + 'h6').length > 0){
-        return { result : true };
+        return { result : "PASS" };
     }
-    return { result : false };
+    return { result : "FAIL" };
 }
 
 const hasTagsAboveH4 = ($) => {
     if ($(globalSelector + 'h3').length > 0 || $(globalSelector + 'h2').length > 0 || $(globalSelector + 'h1').length > 0){
-        return { result : true };
+        return { result : "FAIL" };
     }
-    return { result : false };
+    return { result : "PASS" };
 }
 
 const tablesHasCaption = ($) => {
     if ($(globalSelector + 'table').length > 0 && $(globalSelector + 'table').children('caption').length > 0) {
-        return { result : true };
+        return { result : "PASS" };
     }
-    return { result : false };
+    return { result : "FAIL" };
 }
 
 const searchForSocialSharingElements = ($) => {
-    let result = false;
+    let result = "FAIL";
     if (($(globalSelector + '*[class*="facebook"]').length > 0) || ($(globalSelector + '*[class*="instagram"]').length > 0) || ($(globalSelector + '*[class*="linkedin"]').length > 0) || ($(globalSelector + '*[class*="twitter"]').length > 0)) {
-        result = true;
+        result = "PASS";
     }
     return { result : result };
 }
 
 const testMethodAlwaysFalse = ($) => {
-    return { result : false };
+    return { result : "FAIL" };
 }
 
 export {
